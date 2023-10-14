c2999e2139191f1cc352bb4b11d757413cb31955
Ian Molander <ian.molander@gmail.com>
2023-10-14 16:01:58 +0200

    Init

---
+++ src/main/java/se/iamo/buslineanalyzer/BuslineanalyzerApplication.java
@@ -0,0 +1,13 @@
+package se.iamo.buslineanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuslineanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuslineanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iamo/buslineanalyzer/BuslineanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iamo.buslineanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuslineanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
